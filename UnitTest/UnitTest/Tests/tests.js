b59cf0d275826d445dd5ba3bea7a703d0fc18262
chrisfewtrell <chris.fewtrell@grantadesign.com>
2013-02-13 22:33:13 +0000

    Added tests

--- UnitTest/UnitTest/Tests/tests.js
+++ UnitTest/UnitTest/Tests/tests.js
@@ -1,5 +1,17 @@
 ﻿// Resharper recognizes QUnit (and Jasmine tests) and will run them using test runner.
 // This is fine for testing logic ... but what about testing interactions with the DOM?
-test("hello test", function () {
-    ok(1 == "1", "Passed!");
+
+module("My first module");
+
+// Tests are defined using the test function
+test("Description", function () { });
+
+test("If this fails then all bets are off.", function () {
+    ok(true, "Passed!");
 });
+
+test("hello test", function () {
+    ok(1 == "1", "1 == '1'");
+});
+
+
