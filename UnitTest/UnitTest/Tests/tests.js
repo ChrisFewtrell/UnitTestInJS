d3e89b2e6f4a5b97c8b740eefeee0500d00f5556
chrisfewtrell <chris.fewtrell@grantadesign.com>
2013-02-13 22:23:50 +0000

    Addded minimal unit test

---
+++ UnitTest/UnitTest/Tests/tests.js
@@ -0,0 +1,5 @@
+﻿// Resharper recognizes QUnit (and Jasmine tests) and will run them using test runner.
+// This is fine for testing logic ... but what about testing interactions with the DOM?
+test("hello test", function () {
+    ok(1 == "1", "Passed!");
+});
